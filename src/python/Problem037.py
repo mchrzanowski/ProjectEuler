f3ac6449697385620789278a2b10e3dec2289050
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-01-21 11:41:15 -0500

    p37 speed fix

--- src/python/Problem037.py
+++ src/python/Problem037.py
@@ -8,6 +8,8 @@ from time import time
 from ProjectEulerConstants import isPrime
 
 SOLUTION_LIMIT = 11     # we know there to only exist 11 truncatable primes
+SINGLE_DIGIT_PRIME_SET = set(['2','3','5','7'])     # list of single-digit primes.
+DOUBLE_DIGIT_PRIME_SET = set(['23','29','31','37','41','43','47','53','59','61','67','71','73','79'])   #relevant 2-digit primes.
 
 def produceNumbersFromRightTruncation(number):
     number = str(number)
@@ -18,6 +20,19 @@ def produceNumbersFromLeftTruncation(number):
     number = str(number)
     for i in xrange(1, len(number)):
         yield int(number[i:])
+        
+def checkCandidateWorthiness(number):
+    if '0' in number:
+        return False
+    if number[-1] not in SINGLE_DIGIT_PRIME_SET:
+        return False
+    if number[0] not in SINGLE_DIGIT_PRIME_SET:
+        return False
+    if number[:2] not in DOUBLE_DIGIT_PRIME_SET:
+        return False
+    if not isPrime(number):
+        return False
+    return True
 
 def main():
     
@@ -25,11 +40,11 @@ def main():
    
     primeSet = set([])
     solutionSet = set([])
-    candidate = 11           # skip 2,3,5,7 as per the question.
+    candidate = str(11)           # skip 2,3,5,7 as per the question.
     
     while len(solutionSet) != SOLUTION_LIMIT:
                 
-        if '0' not in str(candidate) and isPrime(candidate):
+        if checkCandidateWorthiness(candidate):
             
             
             isPrimeFromLeftTruncation = True
@@ -54,9 +69,9 @@ def main():
                             break
             
             if isPrimeFromLeftTruncation and isPrimeFromRightTruncation:
-                solutionSet.add(candidate)
+                solutionSet.add(int(candidate))
                 
-        candidate += 1
+        candidate = str(int(candidate) + 1)
         
     end = time()
     
