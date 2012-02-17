c85978c13cf7feb84c9bc2ddc3ff15608a6665c9
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-02-17 17:40:15 -0500

    p80

---
+++ src/python/Problem080.py
@@ -0,0 +1,45 @@
+'''
+Created on Feb 17, 2012
+
+@author: mchrzanowski
+'''
+
+from decimal import getcontext, Decimal
+from math import floor, sqrt
+from time import time
+
+
+LIMIT = 10 ** 2
+
+def sumUpDigits(number):
+    
+    getcontext().prec = 102
+    
+    root = Decimal(number).sqrt()
+    
+    if root // 1 == root:
+        return 0
+    
+    sumOfDigits = int(root)
+    
+    root -= int(root)    
+    
+    for digit in str(root)[2 : LIMIT + 1]:
+        sumOfDigits += int(digit)
+        
+    return sumOfDigits
+    
+
+def main():
+    start = time()
+    sum = 0
+    for i in xrange(1, LIMIT + 1):
+        sum += sumUpDigits(i)
+    
+    print "Sum of first 100 digits in irrational sqrts of numbers <=", LIMIT, " : ", sum
+    
+    end = time()
+    print "Runtime: ", end - start, " seconds."
+
+if __name__ == '__main__':
+    main()
