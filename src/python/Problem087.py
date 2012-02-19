0c8f5fae284c4befcdcb5e4c722a403965d5c74a
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-02-19 01:04:14 -0500

    p87. meh.

---
+++ src/python/Problem087.py
@@ -0,0 +1,49 @@
+'''
+Created on Feb 19, 2012
+
+@author: mchrzanowski
+'''
+
+from ProjectEulerPrime import ProjectEulerPrime
+from math import sqrt
+from time import time
+
+LIMIT = 5 * 10 ** 7
+
+def main():
+    
+    start = time()
+    
+    primeObject = ProjectEulerPrime()
+    
+    solutionSet = set([])
+    
+    squareList = [int(number) ** 2 for number in xrange(2, int(LIMIT ** 0.5) + 1) if primeObject.isPrime(number)]
+    cubeList = [int(number) ** 3 for number in xrange(2, int(LIMIT ** (1./3)) + 1) if primeObject.isPrime(number)]
+    fourthList = [int(number) ** 4 for number in xrange(2, int(LIMIT ** (1./4)) + 1) if primeObject.isPrime(number)]
+    
+    for square in squareList:
+        
+        for cube in cubeList:
+            
+            if square + cube > LIMIT:
+                break
+            
+            for fourth in fourthList:
+                
+                resultingSum = square + cube + fourth
+                
+                if resultingSum > LIMIT:
+                    break
+                
+                else:
+                    solutionSet.add(resultingSum)
+                    
+    print "Number of sums formed from squared, cubed, and fourthed primes: ", len(solutionSet)
+    end = time()
+    print "Runtime: ", end - start, " seconds."
+    
+    
+
+if __name__ == '__main__':
+    main()
