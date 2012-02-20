29b933a6f177e359c72d460cd95f83c23f98cf84
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-02-20 18:56:48 -0500

    this algo is actually incorrect; we implement Euler's theorem incorrectly.

--- src/python/Problem188.py
+++ src/python/Problem188.py
@@ -3,6 +3,7 @@ Created on Feb 19, 2012
 
 @author: mchrzanowski
 '''
+from ProjectEulerLibrary import phi
 from time import time
 
 DIGITS_TO_SEE = 8
@@ -15,15 +16,21 @@ def main():
     A = 1777
     B = 1855
     
+    phiLimit = phi(LIMIT)
+    
     '''
     we use efficient modular exponentiation to do this.
     essentially, there are several highly efficient ways 
     of computing (a ^ b) mod n. Python just so happens to use 
     binary exponentiation in its built-in pow() function.
     http://en.wikipedia.org/wiki/Modular_exponentiation
+    
+    we also use Euler's theorem:
+    http://en.wikipedia.org/wiki/Euler's_theorem
     '''
     residual = A
-    for i in xrange(B - 1): residual = pow(A, residual, LIMIT) 
+    for i in xrange(B - 2): residual = pow(A, residual, phiLimit) 
+    residual = pow(A, residual, LIMIT)
     
     print "Last ", DIGITS_TO_SEE, " digits from the tetration (", A, ", ", B, "): ", residual
     end = time()
