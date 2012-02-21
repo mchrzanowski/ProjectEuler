24a351114ce7422cc6a5cafc440a687829b73846
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-02-20 19:06:55 -0500

    p282. insert memoization for runtime reduction from 16 mins to 0.5 min in pypy 1.8

--- src/python/Problem282.py
+++ src/python/Problem282.py
@@ -16,36 +16,50 @@ def main():
     MOD_LIMIT = 14 ** 8
     PHI_LIMIT = phi(MOD_LIMIT)
     
+    powDict = {}
+    
     for i in xrange(LOOP_LIMIT + 1): 
-        newSolution = ackermann(i, MOD_LIMIT, PHI_LIMIT)
+        newSolution = ackermann(i, MOD_LIMIT, PHI_LIMIT, powDict)
         print i, " : ", newSolution
         solution += newSolution
     
-    print "Solution: ", solution % LIMIT
+    print "Solution: ", solution % MOD_LIMIT
     
     end = time()
     print "Runtime: ", end - start, " seconds."
 
-def tetration(A, B, modulus):
+def tetration(A, B, modulus, powDict):
     
     # tetration. 2 knuth arrows.
     residual = A
     
-    for i in xrange(B - 1): residual = pow(A, residual, modulus) 
+    for i in xrange(B - 1):
+        if residual not in powDict:
+            newResidual = pow(A, residual, modulus)
+            powDict[residual] = newResidual
+            residual = newResidual
+        else:
+            residual = powDict[residual] 
     
     return residual
 
-def pentation(A, B, modulo, phiModulo):
+def pentation(A, B, modulo, phiModulo, powDict):
     
     # pentation. 3 arrows.
-    powerTower = tetration(A, B - 1, phiModulo)
+    powerTower = tetration(A, B - 1, phiModulo, powDict)
     
     residual = A
-    for j in xrange(powerTower - 2): residual = pow(A, residual, phiModulo)
+    for j in xrange(powerTower - 2): 
+        if residual not in powDict:
+            newResidual = pow(A, residual, phiModulo)
+            powDict[residual] = newResidual
+            residual = newResidual
+        else:
+            residual = powDict[residual] 
     
     return residual
 
-def ackermann(number, modulo, phiModulo):
+def ackermann(number, modulo, phiModulo, powDict):
     
     A = 2
 
@@ -68,22 +82,28 @@ def ackermann(number, modulo, phiModulo):
 
     elif number == 4:
         
-        residual = tetration(A, number + 2, phiModulo)
+        residual = tetration(A, number + 2, phiModulo, powDict)
         return pow(A, residual, modulo) - 3
     
     elif number == 5:
         
-        residual =  pentation(A, number + 3, modulo, phiModulo)
+        residual =  pentation(A, number + 3, modulo, phiModulo, powDict)
         return pow(A, residual, modulo) - 3
                 
     elif number == 6:
-        
+                
         # heptation. 4 (omg..) knuth arrows.
-        powerTower = pentation(A, number + 3, modulo, phiModulo)
+        powerTower = pentation(A, number + 3, modulo, phiModulo, powDict)
                 
         residual = A
         
-        for i in xrange(powerTower - 3): residual = pow(A, residual, phiModulo) 
+        for i in xrange(powerTower - 3): 
+            if residual not in powDict:
+                newResidual = pow(A, residual, phiModulo)
+                powDict[residual] = newResidual
+                residual = newResidual
+            else:
+                residual = powDict[residual]
         
         return pow(A, residual, modulo) - 3
     
