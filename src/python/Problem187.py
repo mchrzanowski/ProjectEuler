3e1a34d66d49f1a4c6fc4d532c60c6b998b1e0f4
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-02-22 20:30:15 -0500

    p187. currently taking around 20 seconds in pypy/python2.7

---
+++ src/python/Problem187.py
@@ -0,0 +1,56 @@
+'''
+Created on Feb 21, 2012
+
+@author: mchrzanowski
+'''
+from ProjectEulerPrime import ProjectEulerPrime
+from math import sqrt
+from time import time
+
+LIMIT = 10 ** 8
+
+''' naitve implementation of the prime counting function'''
+def pi(number, storedList=[]):
+            
+    if len(storedList) < number:
+        
+        storedList[:] = [] 
+        
+        storedList.extend([0 for number in xrange(number + 1)])
+    
+        storedList[0] = storedList[1] = 1
+    
+        for i in xrange(2, int(sqrt(number)) + 1):
+            currentValue = i + i
+            while currentValue < len(storedList):
+                storedList[currentValue] = 1
+                currentValue += i
+        
+    return len([storedList[number] for number in xrange(number + 1) if storedList[number] == 0])
+    
+
+def main():
+    '''
+    formula given by MathWorld:
+    http://mathworld.wolfram.com/Semiprime.html
+    '''
+
+    primeObject = ProjectEulerPrime()
+    
+    # first, get all primes < sqrt(LIMIT). place a 0 at index 0 for easier indexing.
+    primesFound = [0, 2]
+    for i in xrange(3, int(sqrt(LIMIT)) + 1, 2):
+        if primeObject.isPrime(i):
+            primesFound.append(i)
+    
+    solution = 0
+    for i in xrange(1, len(primesFound)): solution += pi(LIMIT / primesFound[i]) - i + 1 
+            
+    print "Found:", solution, "composites formed from 2 primes."
+                
+    
+if __name__ == '__main__':
+    start = time()
+    main()
+    end = time()
+    print "Runtime: ", end - start, " seconds."
