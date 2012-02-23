e23e2f06ed37a68b4454a4d618b903ed9ba7d895
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-02-22 20:46:37 -0500

    removing all use of ProjectEulerPrime() as it's faster to just use the sieve algo, it turns out.

--- src/python/Problem187.py
+++ src/python/Problem187.py
@@ -3,17 +3,20 @@ Created on Feb 21, 2012
 
 @author: mchrzanowski
 '''
-from ProjectEulerPrime import ProjectEulerPrime
 from math import sqrt
 from time import time
 
 LIMIT = 10 ** 8
 
 ''' naive implementation of the prime counting function'''
-def pi(number, storedList=[]):
+def pi(number):
     
-    number = int(number)
-            
+    return len(sieveOfEratosthenes(number))
+
+
+''' naive implementation of sieve algo '''
+def sieveOfEratosthenes(number, storedList=[]):
+    
     if len(storedList) < number:
         
         del storedList[:]
@@ -29,22 +32,17 @@ def pi(number, storedList=[]):
                 storedList[currentValue] = 1
                 currentValue += i
         
-    return len([storedList[number] for number in xrange(number + 1) if storedList[number] == 0])
-    
+    return [number for number in xrange(number + 1) if storedList[number] == 0]
 
 def main():
     '''
     formula given by MathWorld:
     http://mathworld.wolfram.com/Semiprime.html
     '''
-
-    primeObject = ProjectEulerPrime()
     
     # first, get all primes < sqrt(LIMIT). place a 0 at index 0 for easier indexing.
-    primesFound = [0, 2]
-    for i in xrange(3, int(sqrt(LIMIT)) + 1, 2):
-        if primeObject.isPrime(i):
-            primesFound.append(i)
+    primesFound = [0]
+    primesFound.extend(sieveOfEratosthenes(sqrt(LIMIT)))
     
     solution = 0
     for i in xrange(1, len(primesFound)): solution += pi(LIMIT / primesFound[i]) - i + 1 
