5da38c5100d728f22b3abe4e6027ddf86c5c9df6
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-03-10 20:01:08 -0500

    modifying P66 to make the convergent pair logic into a modular generator

--- src/python/Problem066.py
+++ src/python/Problem066.py
@@ -12,6 +12,25 @@ from time import time
 LIMIT = 10 ** 3
 
 
+def getConvergentPair(i, difference=1):
+    ''' a generator that will return iterations of increasingly-precise numerator,denominator tuples
+    of the irrational number i '''
+    quotientList = getQuotients(i)      # get the quotient list. this method will only return up to one full period. 
+                                        # we might need more to get convergence, though.
+    
+    convergentState = 0                 # expansion of the quotient list causes some state to be reset.
+                                        # save state because we don't want to send convergents that we've already sent     
+    while True:
+    
+        quotientList.extend(quotientList[1:])       # here, we just double the number of periods in the list. The first value is the integer portion.
+        
+        numerators, denominators = findRationalApproximation(quotientList)  # indexed numerator, denominator dicts
+        
+        for j in xrange(convergentState, len(numerators) - 1):   # these dicts are weirdly indexed; they begin at -1.
+            
+            if numerators[j] ** 2 - i * denominators[j] ** 2 == difference:
+                yield numerators[j], denominators[j]
+                convergentState = j + 1
 
 def main():
     ''' 
@@ -30,27 +49,13 @@ def main():
         
         if sqrt(i).is_integer(): continue   # skip perfect squares.
         
-        quotientList = getQuotients(i)      # get the quotient list. this method will only return up to one full period. 
-                                            # we might need more to get convergence, though.
-        hasNotConvergedYet = True
-        
-        while hasNotConvergedYet:
-        
-            quotientList.extend(quotientList[1:])       # here, we just double the number of periods in the list. The first value is the integer portion.
-            
-            numerators, denominators = findRationalApproximation(quotientList)  # indexed numerator, denominator dicts
-            
-            for j in xrange(len(numerators) - 1):   # these dicts are weirdly indexed; they begin at -1.
+        numerator, denominator = getConvergentPair(i).next()    # first pair will do as we want minimal solutions.
+                            
+        if numerator > maxX:
+            maxX = numerator
+            maxD = i
                 
-                if numerators[j] ** 2 - i * denominators[j] ** 2 == 1:
-                                        
-                    if numerators[j] > maxX:
-                        maxX = numerators[j]
-                        maxD = i
-                        
-                    hasNotConvergedYet = False
-                    break
-    
+
     print "D <=", LIMIT, "that maximizes x:", maxD
     
     end = time()
