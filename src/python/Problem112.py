a887f0adf09c3be33052e4f17fc3fe134ab9219b
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-03-15 21:23:10 -0400

    p112. so easy!

---
+++ src/python/Problem112.py
@@ -0,0 +1,39 @@
+'''
+Created on Mar 15, 2012
+
+@author: mchrzanowski
+'''
+from time import time
+
+def main():
+    
+    LIMIT = 0.99
+    
+    iteration = 101
+    bouncies = 0
+    
+    while float(bouncies) / iteration != LIMIT:
+        
+        strIteration = str(iteration)
+        monoIncrease = monoDecrease = True
+        
+        for digitPlace in xrange(0, len(strIteration) - 1):
+            if strIteration[digitPlace] < strIteration[digitPlace + 1]:
+                monoDecrease = False
+            elif strIteration[digitPlace] > strIteration[digitPlace + 1]:
+                monoIncrease = False
+            
+            if not monoIncrease and not monoDecrease:
+                bouncies += 1
+                break
+        
+        iteration += 1
+
+    print "Number for which ", (LIMIT * 100), "% of numbers are bouncies:",
+    print iteration
+
+if __name__ == '__main__':
+    start = time()
+    main()
+    end = time()
+    print "Runtime:", end - start, "seconds."
