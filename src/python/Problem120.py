f64851dda592d721ac08b775f8c78bd933f1fffe
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-03-20 20:16:51 -0400

    remove special treatment for primes

--- src/python/Problem120.py
+++ src/python/Problem120.py
@@ -4,23 +4,19 @@ Created on Mar 20, 2012
 @author: mchrzanowski
 '''
 
-from ProjectEulerPrime import ProjectEulerPrime
 from time import time
 
 def main():
     ''' 
     this was solved purely through empirical analysis.
-    the largest mod value for primes was found through the formula:
-        incrementor * i + i
-    where incrementor is just a counter that starts at 1 for 3.
-    for even numbers, it turned out to be:
+    the largest mod value for even numbers was found through the formula:
         incrementor * i
-    and odd composites, strangely:
+    where incrementor is just a counter that starts at 1 for 3.        
+    and odd numbers, strangely:
         i * (i - 1)
     '''
     LIMIT = 10 ** 3
     
-    p = ProjectEulerPrime()
     
     solutions = 0
     
@@ -28,10 +24,7 @@ def main():
     
     for i in xrange(3, LIMIT + 1):
         
-        if p.isPrime(i):
-            solutions += incrementor * i + i
-            
-        elif i & 1 == 0:
+        if i & 1 == 0:
             solutions += incrementor * i
             
         else:
