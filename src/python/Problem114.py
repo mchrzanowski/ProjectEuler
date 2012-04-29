6c0d6662e1df2797f477f917804deec74f8b4843
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-04-28 20:21:52 -0400

    p114 mod: increase the loop beginning once more, this time to 6 so as to start the base cases at 1 (0 having a value of 1 is weird: how can it be 1 if there are 0 blocks?)

--- src/python/Problem114.py
+++ src/python/Problem114.py
@@ -9,9 +9,9 @@ from time import time
 def main(): 
     
     LIMIT = 50
-    countingDict = {0:1, 1:1, 2:1, 3:2, 4:4}  # base cases.
+    countingDict = {1:1, 2:1, 3:2, 4:4, 5:7}  # base cases.
     
-    for i in xrange(5, LIMIT + 1):
+    for i in xrange(6, LIMIT + 1):
         countingDict[i] = countingDict[i - 1] + countingDict[i - 2] - countingDict[i - 3] + countingDict[i - 4] + countingDict[i - 5]
     
     print "Solution:", countingDict[LIMIT]
