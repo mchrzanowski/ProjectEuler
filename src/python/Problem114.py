8a51fd3b4489327f688c1223f9d60b695aa448d4
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-04-28 20:05:26 -0400

    p114

---
+++ src/python/Problem114.py
@@ -0,0 +1,25 @@
+'''
+Created on Apr 28, 2012
+
+@author: mchrzanowski
+'''
+
+from time import time
+
+def main(): 
+    
+    LIMIT = 50
+    countingDict = {-1: 1, 0: 1, 1: 1, 2: 1, 3: 2}
+    
+    for i in xrange(4, LIMIT + 1):
+        countingDict[i] = countingDict[i - 1] + countingDict[i - 2] - countingDict[i - 3] + countingDict[i - 4] + countingDict[i - 5]
+        
+#        print i, ":", countingDict[i]                    
+    
+    print "Solution:", countingDict[LIMIT]
+
+if __name__ == '__main__':
+    start = time()
+    main()
+    end = time()
+    print "Runtime:", end - start, "seconds."
