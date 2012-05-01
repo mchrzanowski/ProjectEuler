2667d650364e3ee06f73c4291f978fdb06738184
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-05-01 00:34:02 -0400

    p86. terrifically slow. there must be a faster way.

---
+++ src/python/Problem086.py
@@ -0,0 +1,38 @@
+'''
+Created on Apr 30, 2012
+
+@author: mchrzanowski
+
+USE PYPY ONLY
+'''
+
+from math import sqrt
+from time import time
+
+    
+def main(): 
+    
+    M = 1818
+    solutions = 0
+    
+    for i in xrange(1, M + 1):
+        for j in xrange(i, M + 1):
+            for k in xrange(j, M + 1):
+                
+                # three strategies
+                
+                one = i ** 2 + (j + k) ** 2
+                two = j ** 2 + (i + k) ** 2
+                three = k ** 2 + (i + j) ** 2
+                                
+                if sqrt(min([one, two, three])).is_integer():
+                    solutions += 1
+                    
+    print M, ":", solutions
+                
+
+if __name__ == '__main__':
+    start = time()
+    main()
+    end = time()
+    print "Runtime:", end - start, "seconds."
