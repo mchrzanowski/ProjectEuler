05c5b5f5e5a9d450cd86275a9b7fe3be7644a382
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-05-10 23:57:58 -0400

    p94 logic mod for easier groking. that means more re-use of algos

--- src/python/Problem094.py
+++ src/python/Problem094.py
@@ -13,6 +13,22 @@ def recurrenceRelation(firstResult, secondResult, thirdResult):
         memoizedDict[iterator] = 15 * memoizedDict[iterator - 1] - 15 * memoizedDict[iterator - 2] + memoizedDict[iterator - 3]
         yield memoizedDict[iterator]
         iterator += 1
+        
+def constructProperPerimeterSolutions(defaultSides, recurrenceRelation, perimeterConstruction, LIMIT):
+    solutions = 0
+    
+    for defaultSide in defaultSides:
+        solutions += perimeterConstruction(defaultSide)
+        
+    for newSide in recurrenceRelation:
+        perimeter = perimeterConstruction(newSide)
+        if perimeter <= LIMIT:
+            solutions += perimeter
+        else:
+            break
+        
+    return solutions
+    
 
 def main():
     
@@ -25,39 +41,24 @@ def main():
     # first, calculate the n, n, n+1 heronian isosceles triangles.
     # seed the recurrence relation method with the first 3 n I found by hand that 
     # create valid triangles   
-    defaultNNNPlusOneSides = (5, 65, 901)     
-    nNNPlusOne = recurrenceRelation(defaultNNNPlusOneSides[0], defaultNNNPlusOneSides[1], defaultNNNPlusOneSides[2])
+    defaultNNNPlusOneSides = (5, 65, 901) 
+    nNNPlusOne = recurrenceRelation(*defaultNNNPlusOneSides)
     createPerimeterForNNNPlusOne = lambda x: x * 3 + 1
     
-    for defaultSide in defaultNNNPlusOneSides:
-        solutions += createPerimeterForNNNPlusOne(defaultSide)
-        
-    for newSide in nNNPlusOne:
-        if createPerimeterForNNNPlusOne(newSide) <= LIMIT:
-            solutions += createPerimeterForNNNPlusOne(newSide)
-        else:
-            break
+    solutions += constructProperPerimeterSolutions(defaultNNNPlusOneSides, nNNPlusOne, createPerimeterForNNNPlusOne, LIMIT)
     
     # now, calculate the n, n, n-1 triangles.
     defaultNNNMinusOneSides = (17, 241, 3361)
-    nNNMinusOne = recurrenceRelation(defaultNNNMinusOneSides[0], defaultNNNMinusOneSides[1], defaultNNNMinusOneSides[2])
-    createPerimeterForNNNMinusOne = lambda x: x * 3 - 1
+    nNNMinusOne = recurrenceRelation(*defaultNNNMinusOneSides)
+    createPerimeterForNNNMinusOne = lambda x: x * 3 - 1   
     
-    for defaultSide in defaultNNNMinusOneSides:
-        solutions += createPerimeterForNNNMinusOne(defaultSide)
+    solutions += constructProperPerimeterSolutions(defaultNNNMinusOneSides, nNNMinusOne, createPerimeterForNNNMinusOne, LIMIT)
     
-    for newSide in nNNMinusOne:
-        if createPerimeterForNNNMinusOne(newSide) <= LIMIT:
-            solutions += createPerimeterForNNNMinusOne(newSide)
-        else:
-            break
-
-
     print "Solutions:", solutions    
         
 
 if __name__ == '__main__':
     start = time()
     main()
-    end= time()
+    end = time()
     print "Runtime:", end - start, "seconds."
