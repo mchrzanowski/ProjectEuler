00e8edee5d1d01253055ab9b0dbefeac97db79f1
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-05-15 09:56:56 -0400

    p107. works, and it's fast (but stil O(V^2))

--- src/python/Problem107.py
+++ src/python/Problem107.py
@@ -24,47 +24,45 @@ def readInMatrix(fileName):
     
     return matrix
 
-def connectNewVertexToMST(matrix, mstVertices):
-    
-    minimumWeightedEdge = DEFAULT_NUMBER
-    vertexInMST = None
-    vertexJoiningMST = None
-    
-    for i in xrange(len(mstVertices)):
-        for j in xrange(len(matrix[i])):
-            if j not in mstVertices and matrix[i][j] < minimumWeightedEdge:
-                minimumWeightedEdge = matrix[i][j]
-                vertexInMST = i
-                vertexJoiningMST = j
-    
-    if vertexJoiningMST is None:
-        raise Exception("No new vertex to add to the MST!")
-    
-    # wipe out this edge from the weight matrix.
-    matrix[vertexInMST][vertexJoiningMST] = DEFAULT_NUMBER
-    matrix[vertexJoiningMST][vertexInMST] = DEFAULT_NUMBER
-    mstVertices.add(vertexJoiningMST)
-    
-    return minimumWeightedEdge
-    
-def main(): 
-    
-    
-    # implementation of Prim's algorithm using adjacency lists (ie, matrix)
-    # http://en.wikipedia.org/wiki/Prim%27s_algorithm
-    
-    # store adjacency lists.
-    matrix = readInMatrix('./requiredFiles/Problem107Network.txt')
+def getTotalEdgeWeightOfMatrix(matrix):
     
     totalEdgeWeight = 0
     
-    # get the total weight of the network.
     for i in xrange(len(matrix)):
         for j in xrange(len(matrix[i])):
             if matrix[i][j] != DEFAULT_NUMBER:
                 totalEdgeWeight += matrix[i][j]
-                
+                    
     totalEdgeWeight /= 2
+    
+    return totalEdgeWeight
+
+def createMST(matrix):
+    ''' Prim's algo '''
+    
+    def connectNewVertexToMST(matrix, mstVertices):
+    
+        minimumWeightedEdge = DEFAULT_NUMBER
+        vertexInMST = None
+        vertexJoiningMST = None
+        
+        for i in mstVertices:
+            for j in xrange(len(matrix[i])):
+                if j not in mstVertices and matrix[i][j] < minimumWeightedEdge:
+                    minimumWeightedEdge = matrix[i][j]
+                    vertexInMST = i
+                    vertexJoiningMST = j
+        
+        if vertexJoiningMST is None:
+            raise Exception("No new vertex to add to the MST!")
+        
+        # wipe out this edge from the weight matrix.
+        matrix[vertexInMST][vertexJoiningMST] = DEFAULT_NUMBER
+        matrix[vertexJoiningMST][vertexInMST] = DEFAULT_NUMBER
+        mstVertices.add(vertexJoiningMST)
+        
+        return minimumWeightedEdge
+    
     
     mstVertices = {0}   # start with the 1st vertex by default
     mstEdgeWeight = 0    
@@ -73,6 +71,23 @@ def main():
     # function returns the edge weight used to connect the newest vertex.
     for _ in xrange(len(matrix) - 1):
         mstEdgeWeight += connectNewVertexToMST(matrix, mstVertices)
+    
+    return mstEdgeWeight
+    
+def main(): 
+    
+    # implementation of Prim's algorithm using adjacency lists (ie, a matrix)
+    # http://en.wikipedia.org/wiki/Prim%27s_algorithm
+    
+    # store adjacency lists.
+    matrix = readInMatrix('./requiredFiles/Problem107Network.txt')
+    
+    # get the total weight of the network.
+    totalEdgeWeight = getTotalEdgeWeightOfMatrix(matrix)
+    
+    # get the weight of a minimum spanning tree containing all vertices in the network
+    mstEdgeWeight = createMST(matrix)
+   
     
     print "Total Edge Weight:", totalEdgeWeight
     print "MST Total Weight:", mstEdgeWeight
