4e095f20556b55afc6727ba1dacb8a108e21e9e3
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-06-01 01:57:20 -0400

    p101. super quick.

---
+++ src/python/Problem101.py
@@ -0,0 +1,92 @@
+'''
+Created on May 31, 2012
+
+@author: mchrzanowski
+'''
+
+from time import time
+
+class Point(object):
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def __eq__(self, other):
+        return other.x == self.x and other.y == self.y
+    
+    def __ne__(self, other):
+        return other.x != self.x or other.y != self.y
+    
+    def __str__(self):
+        return "x:" + str(self.x) + "\ty:" + str(self.y) 
+
+
+def fit_polynomial_function_and_get_next_point(points, new_x):
+    ''' 
+        use the LaGrange polynomial function generator algorithm given at:
+        # http://en.wikipedia.org/wiki/Lagrange_polynomial
+        to fit a function and then get the next point 
+    '''
+    
+    new_y = 0
+    
+    for point in points:
+        
+        x = point.x
+        y = point.y
+        
+        temporary_y = 1
+        
+        for other_point in points:
+            
+            if other_point == point:
+                continue
+            
+            temporary_y *= float(new_x - other_point.x) / (x - other_point.x)
+            
+        new_y += y * temporary_y
+            
+    return Point(new_x, int(round(new_y)))      # assume the nearest integer
+
+
+def true_sequence_generator():
+    number = 1
+    while True:
+        result = 0
+        for power in xrange(0, 10 + 1):
+            result += (-1) ** power * number ** power
+        yield result
+        number += 1
+
+def main(): 
+    
+    
+    bad_terms = 0
+    
+    sequence_generator = enumerate(true_sequence_generator())
+    
+    first_point = sequence_generator.next()
+    
+    list_of_true_points = [Point(first_point[0] + 1, first_point[1])]
+        
+    for x, y in sequence_generator:
+                
+        next_theoretical_point = fit_polynomial_function_and_get_next_point(list_of_true_points, x + 1)
+                
+        list_of_true_points.append(Point(x + 1, y))
+                        
+        if next_theoretical_point.y == list_of_true_points[-1].y:
+            break
+        
+        bad_terms += next_theoretical_point.y
+    
+    
+    print "Sum of bad terms:", bad_terms
+
+if __name__ == '__main__':
+    start = time()
+    main()
+    end = time()
+    print "Runtime:", end - start, "seconds."
+
