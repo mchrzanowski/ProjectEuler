2716247fa519007b7eaaa52f4e289ed257f336d7
Mike Chrzanowski <mike.chrzanowski0@gmail.com>
2012-09-21 16:14:17 -0700

    comments.

--- src/python/Problem135.py
+++ src/python/Problem135.py
@@ -13,18 +13,49 @@ def main(max_n, solution_number):
 
     solutions = dict()
 
+    # we can write x ** 2 - y ** 2 - z ** 2 = n as:
+    # (z + 2 * k) ** 2 - (z + k) ** 2 - z ** 2 == n
+
+    # if you find the partial derivative with respect
+    # to z, you see that z = k is the maximum value of the
+    # function. Furthermore, if you look at the output for small k,
+    # you see that the function is symmetric about the maximum:
+    #   k=2, z=1, n=15
+    #   k=2, z=2, n=16
+    #   k=2, z=3, n=15
+    #   k=2, z=4, n=12
+    #   k=2, z=5, n=7
+    #   k=2, z=6, n=0
+    #   k=3, z=1, n=32
+    #   k=3, z=2, n=35
+    #   k=3, z=3, n=36
+    #   k=3, z=4, n=35
+    #   k=3, z=5, n=32
+    #   k=3, z=6, n=27
+    #   k=3, z=7, n=20
+    #   k=3, z=8, n=11
+    #   k=3, z=9, n=0
+    # Finally, note that the last z value before n = 0 is, for a k:
+    # last_z = 2 + 3 * (k - 1)
+    # Therefore, the strategy is to count from last_z to
+    # the max z. We cache the n values up until then. If
+    # n is >= max_n along the way, we stop and increment k.
+    # We have finished once the last value of z for a given k is >= max_n.
+
+    last_z_is_larger_than_max_n = False
     k = 1
     while True:
 
-        last_z = 2 + (k - 1) * 3
-        if produce_n(last_z, k) >= max_n:
+        if last_z_is_larger_than_max_n:
             break
 
         for z in xrange(2 + (k - 1) * 3, k - 1, -1):
 
             n = produce_n(z, k)
-            #print "k=%d, z=%d, n=%d" % (k, z, n)
+
             if n >= max_n:
+                if z == 2 + (k - 1) * 3:
+                    last_z_is_larger_than_max_n = True
                 break
 
             if n not in solutions:
@@ -36,15 +67,16 @@ def main(max_n, solution_number):
                 solutions[n] += 1
 
             z += 1
+
         k += 1
 
     valuable_keys = 0
     for key in solutions:
         if solutions[key] == solution_number:
             valuable_keys += 1
-            #print key
 
-    print valuable_keys
+    print "Values of n < %d that have %d solutions: %d" % \
+    (max_n, solution_number, valuable_keys)
 
 
 if __name__ == '__main__':
